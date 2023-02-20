999d102918e9002f7d2df7008b825331673e3832
JSTKode <mehdi.cesa@outlook.fr>
2023-02-20 23:02:24 +0100

    update

--- src/components/abouts.jsx
+++ src/components/abouts.jsx
@@ -5,6 +5,7 @@ import { TypingText } from "./CustomTexts";
 import styles from "styles";
 import { fadeIn, staggerContainer } from "../../utils/motion";
 
+
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <div className='gradient-02 z-0' />
--- src/components/comparison.jsx
+++ src/components/comparison.jsx
@@ -1,6 +1,7 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
