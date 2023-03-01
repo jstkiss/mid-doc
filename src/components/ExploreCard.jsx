4941ed6322f47f8608b916efc02473d86e5e6e3d
JSTKode <mehdi.cesa@outlook.fr>
2023-03-01 19:07:56 +0100

    uptade

--- src/components/ExploreCard.jsx
+++ src/components/ExploreCard.jsx
@@ -13,7 +13,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
-    <img
+    <Image
       src={imgUrl}
       alt={title}
       className="absolute w-full h-full object-cover rounded-[24px]"
